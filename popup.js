f746e32dda298f0e873ae4818a3e33f40984c54e
kwak <sal091625@gmail.com>
2025-01-05 14:55:45 +0900

    feat(#5): 기본적인 함수 틀 생성

--- popup.js
+++ popup.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('popup.js 로드 성공');
-  const newsContent = document.getElementById('news-content');
+  let articleTxt = null;
+  const inNewsPage = document.getElementById('in-news-page');
+  const notInNewsPage = document.getElementById('not-in-news-page');
+  const stockList = document.getElementById('stock-list');
+  const inputField = document.querySelector('input');
 
   // 현재 탭의 URL 확인
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -11,19 +14,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
       // 백그라운드에서 메시지 수신
       port.onMessage.addListener((message) => {
-        console.log('팝업에서 받은 메시지:', message);
         if (message.type === 'article_text') {
-          newsContent.innerHTML = `<p>${message.text}</p>`;
-        } else {
-          newsContent.innerHTML = '<p>기사를 불러올 수 없습니다.</p>';
+          articleTxt = message.text;
         }
       });
-
-      // 팝업 열림 알림
       port.postMessage({ type: 'popup_opened' });
+      notInNewsPage.style.display = 'none';
     } else {
       // 뉴스 기사 페이지가 아닌 경우
-      newsContent.innerHTML = '<p>뉴스 기사 페이지에서 실행해주세요.</p>';
+      notInNewsPage.style.display = 'block';
     }
   });
+
+  //키워드 검색
+  function searchKeyword(keyword) {
+    //해당 키워드에 대한 유사 키워드 10개 반환
+    //사용자가 검색 결과 중 하나를 선택 시 해당 키워드 id에 대해 bringStockInfo 호출
+  }
+
+  //키워드 추출
+  function extractKeyword(article) {
+    //현재 페이지의 뉴스 기사를 보내 키워드 정보(keyword_id, keyword) 받음
+    //해당 keyword_id로 bringStockInfo 호출
+  }
+
+  //주식 정보 불러오기
+  function bringStockInfo(keword_id) {
+    //1. keyword_id로 해당 키워드에 대해 높은 가중치를 갖는 종목 3개를 받음
+    //2. 각 종목에 대한 주식 정보(시가, 종가, 고가, 저가, 거래량)를 받아옴
+    //3. UI에 적용함
+    inNewsPage.style.display = 'block';
+  }
 });
